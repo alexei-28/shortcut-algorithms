7d0c10cad8cac320abbe01c27b5d9895bc387a9f
a_subscriber <a_subscriber@mail.ru>
2026-09-13 19:21:29 +0300

    Strings (#13)

---
+++ app/src/main/java/com/gmail/alexei28/shortcut/algorithms/strings/ExcelSheetColumnTitle.java
@@ -0,0 +1,45 @@
+package com.gmail.alexei28.shortcut.algorithms.strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+    168. Excel Sheet Column Title [Easy]
+    https://leetcode.com/problems/excel-sheet-column-title/description/?envType=problem-list-v2&envId=string
+
+    Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+    For example:
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+    ...
+
+    Example 1:
+    Input: columnNumber = 1
+    Output: "A"
+
+    Example 2:
+    Input: columnNumber = 28
+    Output: "AB"
+
+    Example 3:
+    Input: columnNumber = 701
+    Output: "ZY"
+*/
+public class ExcelSheetColumnTitle {
+
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+        while (columnNumber > 0) {
+            columnNumber--;
+            char ch = (char) ('A' + columnNumber % 26);
+            result.append(ch);
+            columnNumber = columnNumber / 26;
+        }
+        return result.reverse().toString();
+    }
+}
